1e0807dd99d65fcc981839b4223f1a2d582564ee
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2024-04-26 22:37:19 +0100

    Update heart_diseases.py

--- heart_diseases.py
+++ heart_diseases.py
@@ -108,54 +108,54 @@ pickle.dump(data_to_save, open(filename, 'wb'))
 
 
 
-import streamlit as st
 import pickle
+import pandas as pd
+import numpy as np
+import streamlit as st
 from tensorflow.keras.models import load_model
+
 model = load_model('heartfailurepred.h5')
 
-st.sidebar.image('pngwing.com.png', width = 300,)
+st.sidebar.image('pngwing.com.png', width=300)
 st.sidebar.markdown('<br>', unsafe_allow_html=True)
 selected_page = st.sidebar.radio('Navigation', ['Home', 'Modeling'])
 
 def HomePage():
     # Streamlit app header
-    st.markdown("<h1 style = 'color: #2B2A4C; text-align: center; font-family:montserrat'>Heart Failure Prediction Model</h1>",unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h6 style = 'margin: -15px; color: #2B2A4C; text-align: center ; font-family:montserrat'>The Heart Failure Prediction Model is a machine learning algorithm designed to forecast the likelihood of heart failure in individuals based on various health-related factors. By analyzing features such as age, Sex, medical history, and lifestyle habits, the model provides valuable insights into the risk of heart failure, allowing for early intervention and preventive measures.</h6>",unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.image('2150941719.jpg',  width = 700)
-    st.markdown('<br>', unsafe_allow_html= True)
+    st.markdown("<h1 style='color: #2B2A4C; text-align: center; font-family: montserrat'>Heart Failure Prediction Model</h1>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h6 style='margin: -15px; color: #2B2A4C; text-align: center; font-family: montserrat'>The Heart Failure Prediction Model is a machine learning algorithm designed to forecast the likelihood of heart failure in individuals based on various health-related factors. By analyzing features such as age, sex, medical history, and lifestyle habits, the model provides valuable insights into the risk of heart failure, allowing for early intervention and preventive measures.</h6>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.image('2150941719.jpg', width=700)
+    st.markdown('<br>', unsafe_allow_html=True)
 
-
-
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h3 style='color: #2B2A4C;text-align: center; font-family:montserrat'>The Model Features</h3>", unsafe_allow_html=True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Sex</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h3 style='color: #2B2A4C; text-align: center; font-family: montserrat'>The Model Features</h3>", unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Sex</h5>", unsafe_allow_html=True)
     st.markdown("<p>Sex refers to the biological sex of the individual, which can have an impact on their susceptibility to diabetes. There are three</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Age</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Age</h5>", unsafe_allow_html=True)
     st.markdown("<p>The Age field in the Heart Diseases Model denotes the individual's chronological age, a key factor in assessing heart disease risk. Age ranges from 28-77 in our dataset.</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Hypertension</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Hypertension</h5>", unsafe_allow_html=True)
     st.markdown("<p>Hypertension is a medical condition in which the blood pressure in the arteries is persistently elevated.</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Heart Diseases</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Heart Diseases</h5>", unsafe_allow_html=True)
     st.markdown("<p>Heart disease is another medical condition that is associated with an increased risk of developing diabetes.</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Smoking history</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Smoking history</h5>", unsafe_allow_html=True)
     st.markdown("<p>Smoking history is also considered a risk factor for diabetes and can exacerbate the complications associated</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Body Mass Index</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Body Mass Index</h5>", unsafe_allow_html=True)
     st.markdown("<p>BMI (Body Mass Index) is a measure of body fat based on weight and height. Higher BMI values are linked to a higher risk</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Hemoglobin A1c</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Hemoglobin A1c</h5>", unsafe_allow_html=True)
     st.markdown("<p>HbA1c (Hemoglobin A1c) level is a measure of a person's average blood sugar level over the past 2-3 months.</p>", unsafe_allow_html=True)
-    st.markdown('<br>', unsafe_allow_html= True)
-    st.markdown("<h5 style='color: #2B2A4C;text-align: left; font-family:montserrat'>Blood glucose level</h3>", unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h5 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Blood glucose level</h5>", unsafe_allow_html=True)
     st.markdown("<p>Blood glucose level refers to the amount of glucose in the bloodstream at a given time. </p>", unsafe_allow_html=True)
-
 
     # Streamlit app footer
     st.markdown("<p style='text-align: LEFT; font-size: 12px;'>Created with ❤️ by the Orpheus </p>", unsafe_allow_html=True)
@@ -168,33 +168,28 @@ def modeling_page():
 
     # st.sidebar.image('pngwing.com (13).png', width = 300,  caption = 'customer and deliver agent info')
 
-
-
 if selected_page == "Home":
     HomePage()
 elif selected_page == "Modeling":
-    st.sidebar.markdown('<br>', unsafe_allow_html= True)
+    st.sidebar.markdown('<br>', unsafe_allow_html=True)
     modeling_page()
 
-
-if selected_page == "Modeling":
     st.sidebar.markdown("Add your input here")
-    Age = st.sidebar.number_input("Age",0,1000)
+    Age = st.sidebar.number_input("Age", 0, 1000)
     Sex = st.sidebar.selectbox("Sex", df['Sex'].unique())
-    Cholesterol = st.sidebar.number_input("Cholesterol",0,1000)
-    MaxHR = st.sidebar.number_input('MaxHR',0,1000)
+    Cholesterol = st.sidebar.number_input("Cholesterol", 0, 1000)
+    MaxHR = st.sidebar.number_input('MaxHR', 0, 1000)
     ChestPainType = st.sidebar.selectbox("ChestPainType", df['ChestPainType'].unique())
-    RestingBP = st.sidebar.number_input("RestingBP",0,1000)
-    Oldpeak = st.sidebar.number_input("Oldpeak",  0.0, 10.0, format="%.1f")      
+    RestingBP = st.sidebar.number_input("RestingBP", 0, 1000)
+    Oldpeak = st.sidebar.number_input("Oldpeak", 0.0, 10.0, format="%.1f")      
     ST_Slope = st.sidebar.selectbox("ST_Slope", df['ST_Slope'].unique())
-    st.sidebar.markdown('<br>', unsafe_allow_html= True)
+    st.sidebar.markdown('<br>', unsafe_allow_html=True)
 
-
-# ['Age', 'Sex', 'Cholesterol', 'MaxHR', 'RestingBP', 'Oldpeak', 'ChestPainType', 'ST_Slope']
+    # ['Age', 'Sex', 'Cholesterol', 'MaxHR', 'RestingBP', 'Oldpeak', 'ChestPainType', 'ST_Slope']
 
     input_variables = pd.DataFrame([{
         'Age': Age,
-        'Sex':Sex,
+        'Sex': Sex,
         'Cholesterol': Cholesterol,
         'MaxHR': MaxHR, 
         'ChestPainType': ChestPainType,
@@ -204,53 +199,35 @@ if selected_page == "Modeling":
         'ST_Slope': ST_Slope
     }])
 
+    # Load the saved model and scalers
+    filename = 'labSca.sav'
+    with open(filename, 'rb') as file:
+        saved_data = pickle.load(file)
 
+    label_encoders = saved_data['label_encoders']
+    scaler = saved_data['scaler']
+    columns_to_encode = saved_data['columns_to_encode']
+    columns_to_scale = saved_data['columns_to_scale']
 
+    # Transform categorical columns using label encoders
+    for col, encoder in label_encoders.items():
+        # Reorder input_variables columns to match the order used during fitting
+        input_variables = input_variables[columns_to_encode + columns_to_scale]
+        input_variables[col] = encoder.transform(input_variables[col])
 
-    st.markdown("<h2 style='text-align: LEFT; color: #z2B2A4C;'>Your Input Appears Here</h2>", unsafe_allow_html=True)
+    # Now input_variables should be ready for prediction
     st.write(input_variables)
-    
-# Assuming input_variables is your DataFrame with data to be transformed
 
-import pickle
-import pandas as pd
-
-# Load the saved model and scalers
-filename = 'labSca.sav'
-with open(filename, 'rb') as file:
-    saved_data = pickle.load(file)
-
-label_encoders = saved_data['label_encoders']
-scaler = saved_data['scaler']
-columns_to_encode = saved_data['columns_to_encode']
-columns_to_scale = saved_data['columns_to_scale']
-
-
-# Transform categorical columns using label encoders
-for col, encoder in label_encoders.items():
-    # Reorder input_variables columns to match the order used during fitting
-    input_variables = input_variables[columns_to_encode + columns_to_scale]
-    input_variables[col] = encoder.transform(input_variables[col])
-
-# Scale numerical columns using the saved scaler
-# input_variables[columns_to_scale] = scaler.transform(input_variables[columns_to_scale])
-
-# Now input_variables should be ready for prediction
-
-
-st.write(input_variables)
-
-
-if st.button('Press To Predict'):
-    st.markdown("<h4 style = 'color: #2B2A4C; text-align: left; font-family: montserrat '>Model Report</h4>", unsafe_allow_html = True)
-    predicted = model.predict(input_variables)
-    st.toast('Predicted Successfully')
-    st.image('check icon.png', width = 100)
-    st.success(f'Model Predicted {int(np.round(predicted))}')
-    if predicted >= 0.5:
-        st.error('High risk of Heart Failure!')
-    else:
-        st.success('Low risk of diabHeart Failure.')
+    if st.button('Press To Predict'):
+        st.markdown("<h4 style='color: #2B2A4C; text-align: left; font-family: montserrat'>Model Report</h4>", unsafe_allow_html=True)
+        predicted = model.predict(input_variables)
+        st.toast('Predicted Successfully')
+        st.image('check icon.png', width=100)
+        st.success(f'Model Predicted {int(np.round(predicted))}')
+        if predicted >= 0.5:
+            st.error('High risk of Heart Failure!')
+        else:
+            st.success('Low risk of diabHeart Failure.')
 
 st.markdown('<hr>', unsafe_allow_html=True)
 
