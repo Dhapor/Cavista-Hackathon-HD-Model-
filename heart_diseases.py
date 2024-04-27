4b9cae3f4976957139268486414a26157d5dc867
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2024-04-27 09:10:31 +0100

    Update heart_diseases.py

--- heart_diseases.py
+++ heart_diseases.py
@@ -227,9 +227,9 @@ if selected_page == "Modeling":
             st.toast('Predicted Successfully')
             st.image('check icon.png', width=100)
             if predicted >= 0.5:
-                st.error(f"{patient_name} is at high risk of Lung Cancer!")
+                st.error(f"{patient_name} you may be at high risk of having Heart Disease!")
             else:
-                st.success(f"{patient_name} is at low risk of Lung Cancer.")
+                st.success(f"{patient_name} is at low risk of Heart Disease")
     else:
         st.warning("Please enter the patient's name.")
 
